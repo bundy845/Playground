105fc32370426e5a99899937e71c31f66c9dedc0
Rick Bundren <bundy845@gmail.com>
2015-07-13 15:38:58 -0400

    cleaned up auto detect code

--- FileEncoding/AutoDetect.cs
+++ FileEncoding/AutoDetect.cs
@@ -22,12 +22,41 @@ namespace FileEncoding
 
             //////////////// First check the low hanging fruit by checking if a
             //////////////// BOM/signature exists (sourced from http://www.unicode.org/faq/utf_bom.html#bom4)
-            if (b.Length >= 4 && b[0] == 0x00 && b[1] == 0x00 && b[2] == 0xFE && b[3] == 0xFF) { text = Encoding.GetEncoding("utf-32BE").GetString(b, 4, b.Length - 4); return Encoding.GetEncoding("utf-32BE"); }  // UTF-32, big-endian 
-            else if (b.Length >= 4 && b[0] == 0xFF && b[1] == 0xFE && b[2] == 0x00 && b[3] == 0x00) { text = Encoding.UTF32.GetString(b, 4, b.Length - 4); return Encoding.UTF32; }    // UTF-32, little-endian
-            else if (b.Length >= 2 && b[0] == 0xFE && b[1] == 0xFF) { text = Encoding.BigEndianUnicode.GetString(b, 2, b.Length - 2); return Encoding.BigEndianUnicode; }     // UTF-16, big-endian
-            else if (b.Length >= 2 && b[0] == 0xFF && b[1] == 0xFE) { text = Encoding.Unicode.GetString(b, 2, b.Length - 2); return Encoding.Unicode; }              // UTF-16, little-endian
-            else if (b.Length >= 3 && b[0] == 0xEF && b[1] == 0xBB && b[2] == 0xBF) { text = Encoding.UTF8.GetString(b, 3, b.Length - 3); return Encoding.UTF8; } // UTF-8
-            else if (b.Length >= 3 && b[0] == 0x2b && b[1] == 0x2f && b[2] == 0x76) { text = Encoding.UTF7.GetString(b, 3, b.Length - 3); return Encoding.UTF7; } // UTF-7
+            if (b.Length >= 4 && b[0] == 0x00 && b[1] == 0x00 && b[2] == 0xFE && b[3] == 0xFF)
+            {
+                text = Encoding.GetEncoding("utf-32BE").GetString(b, 4, b.Length - 4); 
+                return Encoding.GetEncoding("utf-32BE");
+            } // UTF-32, big-endian 
+
+            if (b.Length >= 4 && b[0] == 0xFF && b[1] == 0xFE && b[2] == 0x00 && b[3] == 0x00)
+            {
+                text = Encoding.UTF32.GetString(b, 4, b.Length - 4); 
+                return Encoding.UTF32;
+            }// UTF-32, little-endian
+
+            if (b.Length >= 2 && b[0] == 0xFE && b[1] == 0xFF)
+            {
+                text = Encoding.BigEndianUnicode.GetString(b, 2, b.Length - 2); 
+                return Encoding.BigEndianUnicode;
+            }// UTF-16, big-endian
+
+            if (b.Length >= 2 && b[0] == 0xFF && b[1] == 0xFE)
+            {
+                text = Encoding.Unicode.GetString(b, 2, b.Length - 2); 
+                return Encoding.Unicode;
+            }// UTF-16, little-endian
+
+            if (b.Length >= 3 && b[0] == 0xEF && b[1] == 0xBB && b[2] == 0xBF)
+            {
+                text = Encoding.UTF8.GetString(b, 3, b.Length - 3); 
+                return Encoding.UTF8;
+            } // UTF-8
+
+            if (b.Length >= 3 && b[0] == 0x2b && b[1] == 0x2f && b[2] == 0x76)
+            {
+                text = Encoding.UTF7.GetString(b, 3, b.Length - 3);
+                return Encoding.UTF7;
+            } // UTF-7
 
 
             //////////// If the code reaches here, no BOM/signature was found, so now
@@ -44,8 +73,8 @@ namespace FileEncoding
             // For the below, false positives should be exceedingly rare (and would
             // be either slightly malformed UTF-8 (which would suit our purposes
             // anyway) or 8-bit extended ASCII/UTF-16/32 at a vanishingly long shot).
-            int i = 0;
-            bool utf8 = false;
+            var i = 0;
+            var utf8 = false;
 
             while (i < taster - 4)
             {
@@ -65,18 +94,34 @@ namespace FileEncoding
             // The next check is a heuristic attempt to detect UTF-16 without a BOM.
             // We simply look for zeroes in odd or even byte places, and if a certain
             // threshold is reached, the code is 'probably' UF-16.          
-            double threshold = 0.1; // proportion of chars step 2 which must be zeroed to be diagnosed as utf-16. 0.1 = 10%
-            int count = 0;
-            for (int n = 0; n < taster; n += 2) if (b[n] == 0) count++;
-            if (((double)count) / taster > threshold) { text = Encoding.BigEndianUnicode.GetString(b); return Encoding.BigEndianUnicode; }
+            const double threshold = 0.1; // proportion of chars step 2 which must be zeroed to be diagnosed as utf-16. 0.1 = 10%
+
+            var count = 0;
+            
+            for (var n = 0; n < taster; n += 2) 
+                if (b[n] == 0) 
+                    count++;
+
+            if (((double) count)/taster > threshold)
+            {
+                text = Encoding.BigEndianUnicode.GetString(b); return Encoding.BigEndianUnicode;
+            }
+            
             count = 0;
-            for (int n = 1; n < taster; n += 2) if (b[n] == 0) count++;
-            if (((double)count) / taster > threshold) { text = Encoding.Unicode.GetString(b); return Encoding.Unicode; } // (little-endian)
+
+            for (var n = 1; n < taster; n += 2) 
+                if (b[n] == 0) 
+                    count++;
+
+            if (((double) count)/taster > threshold)
+            {
+                text = Encoding.Unicode.GetString(b); return Encoding.Unicode;
+            } // (little-endian)
 
 
             // Finally, a long shot - let's see if we can find "charset=xyz" or
             // "encoding=xyz" to identify the encoding:
-            for (int n = 0; n < taster - 9; n++)
+            for (var n = 0; n < taster - 9; n++)
             {
                 if (
                     ((b[n + 0] == 'c' || b[n + 0] == 'C') && (b[n + 1] == 'h' || b[n + 1] == 'H') && (b[n + 2] == 'a' || b[n + 2] == 'A') && (b[n + 3] == 'r' || b[n + 3] == 'R') && (b[n + 4] == 's' || b[n + 4] == 'S') && (b[n + 5] == 'e' || b[n + 5] == 'E') && (b[n + 6] == 't' || b[n + 6] == 'T') && (b[n + 7] == '=')) ||
@@ -85,21 +130,24 @@ namespace FileEncoding
                 {
                     if (b[n + 0] == 'c' || b[n + 0] == 'C') n += 8; else n += 9;
                     if (b[n] == '"' || b[n] == '\'') n++;
-                    int oldn = n;
+                    
+                    var oldn = n;
+
                     while (n < taster && (b[n] == '_' || b[n] == '-' || (b[n] >= '0' && b[n] <= '9') || (b[n] >= 'a' && b[n] <= 'z') || (b[n] >= 'A' && b[n] <= 'Z')))
                     { n++; }
-                    byte[] nb = new byte[n - oldn];
+                    
+                    var nb = new byte[n - oldn];
+
                     Array.Copy(b, oldn, nb, 0, n - oldn);
                     try
                     {
-                        string internalEnc = Encoding.ASCII.GetString(nb);
+                        var internalEnc = Encoding.ASCII.GetString(nb);
                         text = Encoding.GetEncoding(internalEnc).GetString(b);
                         return Encoding.GetEncoding(internalEnc);
                     }
                     catch { break; }    // If C# doesn't recognize the name of the encoding, break.
                 }
             }
-
 
             // If all else fails, the encoding is probably (though certainly not
             // definitely) the user's local codepage! One might present to the user a
