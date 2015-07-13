161044cd361f3623da79596e5a1fde94ae9b67cc
Rick Bundren <bundy845@gmail.com>
2015-07-13 07:27:33 -0400

    main proggy done

--- Scrath/Program.cs
+++ Scrath/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CsvHelper;
+using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Scrath
 {
@@ -10,6 +9,28 @@ namespace Scrath
     {
         static void Main(string[] args)
         {
+            
+            var stream = new FileStream(@"TestFiles\Aeromexico Hierarchy V2.csv",FileMode.Open,FileAccess.Read);
+            using (var streamReader = new StreamReader(stream, Encoding.Default))
+            {
+                var csvReader = new CsvReader(streamReader);
+
+                var i = 0;
+                while (csvReader.Read() && i < 10)
+                {
+                    foreach (var field in csvReader.CurrentRecord)
+                    {
+                        Console.Write(field);
+                        Console.Write("\t");
+                    }
+                    Console.WriteLine();
+                    i++;
+                }
+
+                Console.ReadLine();
+
+            }
+
         }
     }
 }
