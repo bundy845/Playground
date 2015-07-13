11070ec1820b7f6f0ae9adfd733829ae8d1d55b2
Rick Bundren <bundy845@gmail.com>
2015-07-13 15:13:14 -0400

    added auto detect solution

--- FileEncoding/Program.cs
+++ FileEncoding/Program.cs
@@ -15,17 +15,33 @@ namespace FileEncoding
             // files saved as ANSI (probably from Excel as a CSV in a country with codepage 1252) are not being read correctly
             // this program shows a file saved as ansi and one as UTF and how they render from the stream
             // it's not pretty
+            // in order to see this correctly in the console you need to modify the console to use a TT font
 
-           SetEncodingAndPrint("Default", Encoding.Default);
-           SetEncodingAndPrint("Ascii", Encoding.ASCII);
-           SetEncodingAndPrint("Unicode", Encoding.Unicode);
-           SetEncodingAndPrint("UTF-8", Encoding.UTF8);
-           SetEncodingAndPrint("UTF-7", Encoding.UTF7);
-           SetEncodingAndPrint("1252 Codepage", Encoding.GetEncoding(1252)); 
-           Console.ReadLine();
+            SetEncodingAndPrint("Default", Encoding.Default);
+            SetEncodingAndPrint("Ascii", Encoding.ASCII);
+            SetEncodingAndPrint("Unicode", Encoding.Unicode);
+            SetEncodingAndPrint("UTF-8", Encoding.UTF8);
+            SetEncodingAndPrint("UTF-7", Encoding.UTF7);
+            SetEncodingAndPrint("1252 Codepage", Encoding.GetEncoding(1252)); 
+            AutoDetectOptions();
+            Console.ReadLine();
         }
 
-        private static void SetEncodingAndPrint( string encodingName, Encoding encoding)
+        private static void AutoDetectOptions()
+        {
+            AutoDetectFile(@"TestFiles\ANSI.csv");
+            AutoDetectFile(@"TestFiles\UTF-8.csv");
+        }
+
+        private static void AutoDetectFile(string file)
+        {
+            string text;
+            var encoding = AutoDetect.DetectTextEncoding(file, out text);
+            Console.WriteLine("{0} detected as {1}",file, encoding.EncodingName);
+            PrintFile(encoding.EncodingName, file, encoding);
+        }
+
+        private static void SetEncodingAndPrint(string encodingName, Encoding encoding)
         {
             PrintFile(encodingName + " - Ansi File", @"TestFiles\ANSI.csv", encoding);
             PrintFile(encodingName + " - UTF-8 File", @"TestFiles\UTF-8.csv", encoding);
